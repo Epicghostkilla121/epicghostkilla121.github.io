40a31a4cda4bcd856a87be359438be12aaa4e11b
Epicghostkilla121 <140558365+Epicghostkilla121@users.noreply.github.com>
2023-08-09 16:33:57 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,20 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const readMoreButtons = document.querySelectorAll(".read-more");
-    
-    readMoreButtons.forEach(button => {
-        button.addEventListener("click", function() {
-            const description = this.previousElementSibling; // Get the description element
-            description.classList.toggle("expanded");
-            this.textContent = description.classList.contains("expanded") ? "Read Less" : "Read More";
+// Add JavaScript code to open modal/popup for detailed information
+document.addEventListener("DOMContentLoaded", function () {
+    const learnMoreLinks = document.querySelectorAll(".learn-more");
+    const modal = document.getElementById("game-modal");
+
+    learnMoreLinks.forEach((link) => {
+        link.addEventListener("click", function (event) {
+            event.preventDefault();
+            modal.style.display = "block";
+            // Load and display detailed game information in the modal
+            // You can use AJAX or predefined content for this
         });
+    });
+
+    modal.addEventListener("click", function (event) {
+        if (event.target === modal) {
+            modal.style.display = "none";
+        }
     });
 });
